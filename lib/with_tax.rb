15f3d29a5f3b0a3579b3d290920d305f5a8e7376
ken1flan <ken1flan@gmail.com>
2020-03-28 17:26:22 +0900

    Add ???_with_tax

--- lib/with_tax.rb
+++ lib/with_tax.rb
@@ -2,5 +2,17 @@ require "with_tax/version"
 
 module WithTax
   class Error < StandardError; end
-  # Your code goes here...
+
+  def method_missing(name, *args)
+    if name.match(/\A(.*)_with_tax\Z/)
+      self.class.class_eval do
+        define_method(name) do
+          (send($1) * 1.10).ceil
+        end
+      end
+      send(name)
+    else
+      super
+    end
+  end
 end
--- spec/with_tax_spec.rb
+++ spec/with_tax_spec.rb
@@ -1,9 +1,31 @@
 RSpec.describe WithTax do
-  it "has a version number" do
-    expect(WithTax::VERSION).not_to be nil
+  describe "WithTax::VERSION" do
+    it "has a version number" do
+      expect(WithTax::VERSION).not_to be nil
+    end
   end
 
-  it "does something useful" do
-    expect(true).to eq(true)
+  describe "#???_with_tax" do
+    context "SampleItem class exist" do
+      class SampleItem
+        include WithTax
+
+        attr_accessor :name, :price
+
+        def initialize(name, price)
+          @name = name
+          @price = price
+        end
+      end
+
+      describe "SampleItem#price_with_tax" do
+        subject { sample_item.price_with_tax }
+
+        let(:sample_item) { SampleItem.new("SampleName", price) }
+        let(:price) { rand(10000) + 1 }
+
+        it { is_expected.to eq (price * 1.10).ceil }
+      end
+    end
   end
 end
