d56c963501560a292a46cef348dbba7a7584d63f
ken1flan <ken1flan@gmail.com>
2020-03-30 06:58:41 +0900

    Add target_date parameter
    税率は年月日によって違うので、
    税率を求める際に指定できるようにした。

--- lib/with_tax/rate.rb
+++ lib/with_tax/rate.rb
@@ -1,7 +1,22 @@
+require 'date'
+
 module WithTax
   class Rate
-    def self.rate
-      0.10
+    def self.rate(target_date = nil)
+      t = target_date
+      t ||= Date.today
+      case t
+      when Date.new...Date.new(1989, 4, 1)
+        0.0
+      when Date.new(1989, 4, 1)...Date.new(1997, 4, 1)
+        0.03
+      when Date.new(1997, 4, 1)...Date.new(2014, 4, 1)
+        0.05
+      when Date.new(2014, 4, 1)...Date.new(2019, 10, 1)
+        0.08
+      else
+        0.10
+      end
     end
   end
 end
--- spec/rate_spec.rb
+++ spec/rate_spec.rb
@@ -1,7 +1,107 @@
 RSpec.describe WithTax::Rate do
   describe ".rate" do
-    subject { WithTax::Rate.rate }
+    subject { WithTax::Rate.rate(target_date) }
 
-    it { is_expected.to eq 0.10 }
+    context "When target_date = nil" do
+      let(:target_date) { nil }
+      before { Timecop.freeze(Date.parse(today)) }
+      after { Timecop.return }
+
+      context "When today is 1989/03/31" do
+        let(:today) { "1989/03/31" }
+
+        it { is_expected.to eq 0.0 }
+      end
+
+      context "When today is 1989/04/01" do
+        let(:today) { "1989/04/01" }
+
+        it { is_expected.to eq 0.03 }
+      end
+
+      context "When today is 1997/03/31" do
+        let(:today) { "1997/03/31" }
+
+        it { is_expected.to eq 0.03 }
+      end
+
+      context "When today is 1997/04/01" do
+        let(:today) { "1997/04/01" }
+
+        it { is_expected.to eq 0.05 }
+      end
+
+      context "When today is 2014/03/31" do
+        let(:today) { "2014/03/31" }
+
+        it { is_expected.to eq 0.05 }
+      end
+
+      context "When today is 2014/04/01" do
+        let(:today) { "2014/04/01" }
+
+        it { is_expected.to eq 0.08 }
+      end
+
+      context "When today is 2019/09/30" do
+        let(:today) { "2019/09/30" }
+
+        it { is_expected.to eq 0.08 }
+      end
+
+      context "When today is 2019/10/01" do
+        let(:today) { "2019/10/01" }
+
+        it { is_expected.to eq 0.10 }
+      end
+    end
+
+    context "When target_date = 1989/03/31" do
+      let(:target_date) { Date.new(1989, 3, 31) }
+
+      it { is_expected.to eq 0.0 }
+    end
+
+    context "When target_date = 1989/04/01" do
+      let(:target_date) { Date.new(1989, 4, 1) }
+
+      it { is_expected.to eq 0.03 }
+    end
+
+    context "When target_date = 1997/03/31" do
+      let(:target_date) { Date.new(1997, 3, 31) }
+
+      it { is_expected.to eq 0.03 }
+    end
+
+    context "When target_date = 1997/04/01" do
+      let(:target_date) { Date.new(1997, 4, 1) }
+
+      it { is_expected.to eq 0.05 }
+    end
+
+    context "When target_date = 2014/03/31" do
+      let(:target_date) { Date.new(2014, 3, 31) }
+
+      it { is_expected.to eq 0.05 }
+    end
+
+    context "When target_date = 2014/04/01" do
+      let(:target_date) { Date.new(2014, 4, 1) }
+
+      it { is_expected.to eq 0.08 }
+    end
+
+    context "When target_date = 2019/09/30" do
+      let(:target_date) { Date.new(2019, 9, 30) }
+
+      it { is_expected.to eq 0.08 }
+    end
+
+    context "When target_date = 2019/10/01" do
+      let(:target_date) { Date.new(2019, 10, 1) }
+
+      it { is_expected.to eq 0.10 }
+    end
   end
 end
