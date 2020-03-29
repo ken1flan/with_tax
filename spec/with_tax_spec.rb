b09e57ad6952d57271f826202eac89ca6483760c
ken1flan <ken1flan@gmail.com>
2020-03-29 15:34:19 +0900

    Add #rounding_method
    小数点以下の処置方法を選べるようにした。
    
    選べる種類は
    - 何もしない nil
    - 切り捨て :floor
    - 四捨五入 :round
    - 切り上げ :ceil

--- spec/with_tax_spec.rb
+++ spec/with_tax_spec.rb
@@ -30,4 +30,74 @@ RSpec.describe WithTax do
 
     it { is_expected.to eq (price * 1.10).ceil }
   end
+
+  describe "#rounding_method" do
+    subject { sample_item.price_with_tax }
+
+    let(:sample_item) { SampleItem.new("SampleName", price) }
+
+    context "When rounding_method = nil" do
+      before { WithTax.rounding_method = nil }
+
+      context "When price = 123" do
+        let(:price) { 123 }
+
+        it { is_expected.to eq 135.3 }
+      end
+
+      context "When price = 345" do
+        let(:price) { 345 }
+
+        it { is_expected.to eq 379.5 }
+      end
+    end
+
+    context "When rounding_method = :floor" do
+      before { WithTax.rounding_method = :floor }
+
+      context "When price = 123" do
+        let(:price) { 123 }
+
+        it { is_expected.to eq 135 }
+      end
+
+      context "When price = 345" do
+        let(:price) { 345 }
+
+        it { is_expected.to eq 379 }
+      end
+    end
+
+    context "When rounding_method = :round" do
+      before { WithTax.rounding_method = :round }
+
+      context "When price = 123" do
+        let(:price) { 123 }
+
+        it { is_expected.to eq 135 }
+      end
+
+      context "When price = 345" do
+        let(:price) { 345 }
+
+        it { is_expected.to eq 380 }
+      end
+    end
+
+    context "When rounding_method = :ceil" do
+      before { WithTax.rounding_method = :ceil }
+
+      context "When price = 123" do
+        let(:price) { 123 }
+
+        it { is_expected.to eq 136 }
+      end
+
+      context "When price = 345" do
+        let(:price) { 345 }
+
+        it { is_expected.to eq 380 }
+      end
+    end
+  end
 end
