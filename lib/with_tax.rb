c0d9d9fb8bd3a9983a64a26259a67925b643395e
ken1flan <ken1flan@gmail.com>
2020-04-11 09:25:48 +0900

    Replace delete_suffix
    String#delete_suffixをやめ、String#subに変更しました。

--- lib/with_tax.rb
+++ lib/with_tax.rb
@@ -19,7 +19,7 @@ module WithTax
   def add_with_tax_method(name)
     self.class.class_eval do
       define_method(name) do
-        ret = send(name.to_s.delete_suffix("_with_tax")).to_s.to_d * (1 + WithTax::Rate.rate(nil, WithTax::Config.rate_type))
+        ret = send(name.to_s.sub(/_with_tax\Z/, '')).to_s.to_d * (1 + WithTax::Rate.rate(nil, WithTax::Config.rate_type))
 
         rounding_method = WithTax::Config.rounding_method
         rounding_method ? ret.send(rounding_method) : ret
