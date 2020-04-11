b4bbfc6e4e31aea7512e60314b94150ed1e6d1b0
ken1flan <ken1flan@gmail.com>
2020-04-11 16:16:13 +0900

    Extract method_definer
    includeするとadd_with_tax_methodも追加されてしまっていたので、
    別のクラスに抽出して、修正した。

--- lib/with_tax.rb
+++ lib/with_tax.rb
@@ -1,30 +1,15 @@
-require 'bigdecimal'
-require 'bigdecimal/util'
 require 'with_tax/version'
-require 'with_tax/rate'
-require 'with_tax/config'
+require 'with_tax/method_definer'
 
 module WithTax
   class Error < StandardError; end
 
   def method_missing(name, *args)
     if name.match(/\A(.*)_with_tax\Z/)
-      add_with_tax_method(name)
+      WithTax::MethodDefiner.add_method(self.class, name)
       send(name, *args)
     else
       super
-    end
-  end
-
-  def add_with_tax_method(name)
-    self.class.class_eval do
-      define_method(name) do |effective_date = nil|
-        mag = 1 + WithTax::Rate.rate(effective_date, WithTax::Config.rate_type)
-        ret = send(name.to_s.sub(/_with_tax\Z/, '')).to_s.to_d * mag
-
-        rounding_method = WithTax::Config.rounding_method
-        rounding_method ? ret.send(rounding_method) : ret
-      end
     end
   end
 end
