073c2fffa2eb38501693e73f4ab506a212ef45e8
ken1flan <ken1flan@gmail.com>
2020-04-11 15:23:44 +0900

    Update add_with_tax_method
    適用日を引数に取るように変更しました。

--- lib/with_tax.rb
+++ lib/with_tax.rb
@@ -10,7 +10,7 @@ module WithTax
   def method_missing(name, *args)
     if name.match(/\A(.*)_with_tax\Z/)
       add_with_tax_method(name)
-      send(name)
+      send(name, *args)
     else
       super
     end
@@ -18,8 +18,9 @@ module WithTax
 
   def add_with_tax_method(name)
     self.class.class_eval do
-      define_method(name) do
-        ret = send(name.to_s.sub(/_with_tax\Z/, '')).to_s.to_d * (1 + WithTax::Rate.rate(nil, WithTax::Config.rate_type))
+      define_method(name) do |effective_date = nil|
+        mag = 1 + WithTax::Rate.rate(effective_date, WithTax::Config.rate_type)
+        ret = send(name.to_s.sub(/_with_tax\Z/, '')).to_s.to_d * mag
 
         rounding_method = WithTax::Config.rounding_method
         rounding_method ? ret.send(rounding_method) : ret
