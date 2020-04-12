94ace9d3d403793a77901b8e2d8196547619a6ad
ken1flan <ken1flan@gmail.com>
2020-04-12 21:21:38 +0900

    Fix method_definer
    method_missingから事前登録式に変えようとしていて、
    そのときは属性名をベースにメソッドを追加するようにするほうが
    やりやすいので、変更しました。

--- lib/with_tax/method_definer.rb
+++ lib/with_tax/method_definer.rb
@@ -5,12 +5,13 @@ require 'with_tax/config'
 
 module WithTax
   class MethodDefiner
-    def self.add_method(klass, name)
+    def self.add_method(klass, attr_name)
       klass.class_eval do
-        define_method(name) do |effective_date = nil|
+        method_name = "#{attr_name}_with_tax"
+        define_method(method_name) do |effective_date = nil|
           rate_type = respond_to?(:with_tax_rate_type) ? with_tax_rate_type : WithTax::Config.rate_type
           mag = 1 + WithTax::Rate.rate(effective_date, rate_type)
-          ret = send(name.to_s.sub(/_with_tax\Z/, '')).to_s.to_d * mag
+          ret = send(attr_name).to_s.to_d * mag
 
           rounding_method = WithTax::Config.rounding_method
           rounding_method ? ret.send(rounding_method) : ret
