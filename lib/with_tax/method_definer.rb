b0cf672a8c352dba437da2d99ee3fc7f2140f839
ken1flan <ken1flan@gmail.com>
2020-04-12 09:56:23 +0900

    Use obj.with_tax_rate_type
    WithTaxがincludeされたオブジェクトに
    with_tax_rate_typeという属性やメソッドがあれば
    それに基づいて税率を計算するようにしました。

--- lib/with_tax/method_definer.rb
+++ lib/with_tax/method_definer.rb
@@ -5,10 +5,11 @@ require 'with_tax/config'
 
 module WithTax
   class MethodDefiner
-    def self.add_method(target_class, name)
-      target_class.class_eval do
+    def self.add_method(obj, name)
+      obj.class.class_eval do
         define_method(name) do |effective_date = nil|
-          mag = 1 + WithTax::Rate.rate(effective_date, WithTax::Config.rate_type)
+          rate_type = obj.respond_to?(:with_tax_rate_type) ? obj.with_tax_rate_type : WithTax::Config.rate_type
+          mag = 1 + WithTax::Rate.rate(effective_date, rate_type)
           ret = send(name.to_s.sub(/_with_tax\Z/, '')).to_s.to_d * mag
 
           rounding_method = WithTax::Config.rounding_method
